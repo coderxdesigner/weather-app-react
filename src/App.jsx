77ac55ebd9dd379d49ba6fc355a675014cddfb39
coderxdesigner <rachel.heneault@gmail.com>
2023-10-28 22:19:55 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+  
+      <div className="card">
+        hello weather
+      </div>
+    
+  )
+}
+
+export default App
